c0537401b15393ee8a1beb95574ad9d9ed76c023
RichardRivaldo <richardrivaldo84@gmail.com>
2021-10-07 23:43:35 +0700

    merged local search file and added methods for local search

--- src/ai/local_search.py
+++ src/ai/local_search.py
@@ -1,11 +1,11 @@
+import random
+from itertools import product
 from math import exp
 from time import time
-from typing import Tuple
-from model.player import Player
+from typing import List, Tuple
 
-from src.constant import GameConstant, ShapeConstant
+from src.constant import ColorConstant, ShapeConstant
 from src.model import Board, State
-from src.utility import is_out, place
 
 
 class LocalSearch:
@@ -13,99 +13,189 @@ class LocalSearch:
         pass
 
     def find(
-        self, state: State, n_player: int, thinking_time: float
-    ) -> Tuple[str, str]:
+            self, state: State, n_player: int, thinking_time: float
+    ) -> Tuple[int, str]:
         self.thinking_time = time() + thinking_time
 
-        if self.is_empty(state.board):
-            return (state.board.col // 2, state.players[n_player].shape)
+        print(self.get_valid_moves(state, n_player))
 
-        best_movement = (
-            self.hill_climbing(state, n_player, thinking_time),
-            state.players[n_player].shape,
-        )
+        best_movement = self.select_random_move(state, n_player)
 
         return best_movement
 
-    def is_valid_move(state: State, piece, column):
-        return place()
+    def countGroupHorizontal(board: Board, row: int, col: int):
+        # return number of pieces in a 4 group horizontally
+        countPiece = 0
+        for i in range(col, col + 4):
+            if board[row, i].shape != ShapeConstant.BLANK:
+                countPiece += 1
+        return countPiece
 
-    def find_possible_moves(state: State):
-        print(state.board)
+    def countGroupVertical(board: Board, row: int, col: int):
+        # return number of pieces in a 4 group horizontally
+        countPiece = 0
+        for i in range(row, row + 4):
+            if board[i, col].shape != ShapeConstant.BLANK:
+                countPiece += 1
+        return countPiece
+
+    def countGroupColorShapeHorizontal(
+            board: Board, color: ColorConstant, shape: ShapeConstant, row: int, col: int
+    ):
+        # return number of pieces in a group that fits either color or shape
+        countPiece = 0
+        for i in range(col, col + 4):
+            if board[row, i].shape == shape or board[row, i].color == color:
+                countPiece += 1
+        return countPiece
+
+    def countGroupColorShapeVertical(
+            board: Board, color: ColorConstant, shape: ShapeConstant, row: int, col: int
+    ):
+        # return number of pieces in a group that fits either color or shape
+        countPiece = 0
+        for i in range(row, row + 4):
+            if board[i, col].shape == shape or board[i, col].color == color:
+                countPiece += 1
+        return countPiece
+
+    def evaluateGroupHorizontal(state: State, n_player: int, i: int, j: int):
+        # return value of group 4 horizontally on position board[i,j]
+        currentBoard = state.board
+        myPlayer = state.players[n_player]
+        enemyPlayer = state.players[
+            (int(not n_player))
+        ]  # not n_player since n_player can only be 1 or 0 will always refer to the other
+        numberOfPiece = LocalSearch.countGroupHorizontal(currentBoard, i, j)
+        countEnemyPiece = LocalSearch.countGroupColorShapeHorizontal(
+            currentBoard, enemyPlayer.color, enemyPlayer.shape, i, j
+        )
+        countPlayerPiece = LocalSearch.countGroupColorShapeHorizontal(
+            currentBoard, myPlayer.color, myPlayer.shape, i, j
+        )
+
+        if numberOfPiece == 4:
+            # if 4 piece of a group are full
+            if countEnemyPiece == 4:
+                return -999
+            else:
+                return +999
+        elif numberOfPiece == 3:
+            # 3 pieces and 1 empty
+            if countEnemyPiece == 3:
+                return -999
+            elif countPlayerPiece == 3:
+                return 10
+            elif countPlayerPiece == 2:
+                return 4
+        elif numberOfPiece == 2:
+            # 2 pieces and 2 empty
+            if countEnemyPiece == 2:
+                return -4
+            else:
+                return 4
 
     @staticmethod
-    def is_empty(board: Board):
-        for row in range(board.row):
-            for col in range(board.col):
-                if board[row, col].shape != ShapeConstant.BLANK:
-                    return False
-        return True
+    def evaluate_group_vertical(state: State, n_player: int, i: int, j: int):
+        current_board = state.board
+        my_player = state.players[n_player]
+        enemy_player = state.players[
+            (int(not n_player))
+        ]  # not n_player since n_player can only be 1 or 0 will always refer to the other
+        number_of_piece = LocalSearch.countGroupVertical(current_board, i, j)
+        count_enemy_piece = LocalSearch.countGroupColorShapeVertical(
+            current_board, enemy_player.color, enemy_player.shape, i, j
+        )
+        count_player_piece = LocalSearch.countGroupColorShapeVertical(
+            current_board, my_player.color, my_player.shape, i, j
+        )
+
+        if number_of_piece == 4:
+            # if 4 piece of a group are full
+            if count_enemy_piece == 4:
+                return -999
+            else:
+                return +999
+        elif number_of_piece == 3:
+            # 3 pieces and 1 empty
+            if count_enemy_piece == 3:
+                return -999
+            elif count_player_piece == 3:
+                return 10
+            elif count_player_piece == 2:
+                return 4
+        elif number_of_piece == 2:
+            # 2 pieces and 2 empty
+            if count_enemy_piece == 2:
+                return -4
+            else:
+                return 4
 
     @staticmethod
-    def utility_function(board: Board, row: int, col: int):
-        # yes i reused check_streak, no im not ashamed
-        """
-        [DESC] fungsi utk dapat value heuristik piece di board[row][col]
-        [RETURN]
-        0 if board[row][col] kosong
-        int if ada isinya
-        """
-        piece = board[row, col]
+    def state_heuristic(state: State, n_player: int):
+        # function that returns a value of heuristic of a given state
+        current_board = state.board
+        my_player = state.players[n_player]
+        enemy_player = state.players[
+            (int(not n_player))
+        ]  # not n_player since n_player can only be 1 or 0 will always refer to the other
 
-        streak_way = [
-            (-1, 0),
-            (1, 0),
-            (0, -1),
-            (0, 1),
-            (-1, -1),
-            (-1, 1),
-            (1, -1),
-            (1, 1),
+        state_value = 0
+        # check horizontally 1 board
+        for i in range(current_board.row):
+            for j in range(0, current_board.col - 4):
+                state_value += LocalSearch.evaluateGroupHorizontal(
+                    state, n_player, i, j
+                )
+        # check vertically 1 board
+        for i in range(current_board.col):
+            for j in range(0, current_board.row - 4):
+                state_value += LocalSearch.evaluate_group_vertical(
+                    state, n_player, i, j
+                )
+
+        return state_value
+
+    # Get empty column to place pieces
+    @staticmethod
+    def get_empty_column(current_state: State) -> List[int]:
+        # Only need check if board[0] is empty (contains blank piece)
+        current_board = current_state.board.board
+        board_first_row = current_board[0]
+
+        return [
+            col
+            for col in range(len(board_first_row))
+            if board_first_row[col].shape == ShapeConstant.BLANK
         ]
-        mark = 0
 
-        for prior in GameConstant.WIN_PRIOR:
-            for row_ax, col_ax in streak_way:
-                row_ = row + row_ax
-                col_ = col + col_ax
-                # since board is 6x7, 10 should be fine to check all cardinal directions
-                for _ in range(10):
-                    if is_out(board, row_, col_):
-                        mark += 0
-                        break
-                    # true if shape different
-                    shape_condition = (
-                        prior == GameConstant.SHAPE
-                        and piece.shape != board[row_, col_].shape
-                    )
-                    # true if color different
-                    color_condition = (
-                        prior == GameConstant.COLOR
-                        and piece.color != board[row_, col_].color
-                    )
-                    # if meet blank piece
-                    if board[row_, col_].shape == ShapeConstant.BLANK:
-                        mark += 0
-                    else:
-                        # same color shape
-                        if (not shape_condition) and (not color_condition):
-                            mark += 3
-                        elif (not shape_condition) and color_condition:  # same shape
-                            mark += 1
-                        elif shape_condition and (not color_condition):  # same color
-                            mark -= 1
-                        elif shape_condition and color_condition:  # none same
-                            mark -= 3
-                    row_ += row_ax
-                    col_ += col_ax
+    # Get pieces which shape is still available
+    @staticmethod
+    def get_available_shape(current_state: State, n_player: int) -> List[str]:
+        current_player = current_state.players[n_player]
+        current_player_quote = current_player.quota
 
-        return mark
+        available_pieces = []
+        for shape in current_player_quote:
+            if current_player_quote[shape] > 0:
+                available_pieces.append(shape)
 
-    def select_random_move():
-        pass
+        return available_pieces
 
-    def calculate_delta_e(current_state, current_value, random_neighbors):
-        pass
+    # Get all valid pairs of column and shape as valid moves
+    def get_valid_moves(
+            self, current_state: State, n_player: int
+    ) -> List[Tuple[int, str]]:
+        valid_columns = self.get_empty_column(current_state)
+        valid_shape = self.get_available_shape(current_state, n_player)
+
+        return list(product(valid_columns, valid_shape))
+
+    def select_random_move(
+            self, current_state: State, n_player: int
+    ) -> Tuple[int, str]:
+        valid_moves = self.get_valid_moves(current_state, n_player)
+        return random.choice(valid_moves)
 
     @staticmethod
     def hill_climbing(state: State, n_player: int, thinking_time: float):
@@ -127,43 +217,38 @@ class LocalSearch:
                 mark = max(current_mark, mark)
         return column_choice
 
-    @staticmethod
-    def state_heuristic(board:Board, player:Player):
-        """
-        [DESC] gives a heuristic value given a board
-        [RETURN] an integer that represents value of current state of board
-        """
-        val = -999
-        for i in range (board.row):
-            for j in range (board.col):
-                if (board[i,j].shape==player.shape and board[i,j].color==player.color):
-                    curVal = LocalSearch.utility_function(board,i,j)
-                    val = max(curVal,val)
+    def calculate_delta_e(self, current_state, current_value, random_successor):
+        return 0
 
-        return val
-
-    def simulated_annealing(self, current_state: State, n_player:int):
+    def simulated_annealing(self, current_state: State, n_player: int):
         # self.thinking_time = 3
-        # self.max_depth = 5?
-        # allocated_time = t + 0.6 / 7
-        # Temperature = current time - allocated time
-        allocated_time = self.thinking_time / (7 * self.max_depth) + time()
-        current_value = LocalSearch.state_heuristic(current_state.board,current_state.players[n_player])
-        available_moves = []
+        # allocated_time = t + 3 -> Takes all the thinking time
+        allocated_time = time() + self.thinking_time
+
+        # Calculate the current state value
+        current_value = LocalSearch.state_heuristic(current_state.players[n_player])
+
+        # Greedy Simulated Annealing -> Find best neighbor that gives highest state value
+        best_move = None
 
         # Iterate until the temperature is cool enough
         while True:
+            # Current temperature = current time - allocated time
             # Check if the current time didnt exceed allocated time, temperature >= 0
             current_temperature = allocated_time - time()
             if current_temperature <= 0:
-                return available_moves
-            random_successor = self.select_random_move()
-            if random_successor:
+                if best_move:
+                    return best_move
+                else:
+                    return self.select_random_move(current_state, n_player)
+
+            random_next_move = self.select_random_move(current_state, n_player)
+            if random_next_move:
                 delta_e = self.calculate_delta_e(
-                    current_state, current_value, random_successor
+                    current_state, current_value, random_next_move
                 )
                 if delta_e > 0:
-                    available_moves.append(random_successor)
+                    best_move = random_next_move
                 else:
                     if exp(delta_e / current_temperature) > 0:
-                        available_moves.append(random_successor)
+                        pass
