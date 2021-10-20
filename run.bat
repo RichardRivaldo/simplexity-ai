26b13ed98d32c9bf65b292230f02143c408e33c4
RichardRivaldo <richardrivaldo84@gmail.com>
2021-10-20 14:05:08 +0700

    change bat file to source bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python main.py --row 6 --col 7 --type pvb --player_choice 0 --thinking_time 3 --bot1 minimax-1.pkl --bot2 random-1.pkl
+python main.py --is_dump --row 6 --col 7 --type bvb --player_choice 0 --thinking_time 3 --bot1 minimax-1.pkl --bot2 random-1.pkl
